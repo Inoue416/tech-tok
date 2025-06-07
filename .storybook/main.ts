8a1caccc9081d1730d82f69a3fd0bda9adaa24b6
i-yuya <yuyatuta@gmail.com>
2025-06-07 20:26:25 +0900

    [ADD] storybook導入

---
+++ .storybook/main.ts
@@ -0,0 +1,10 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+	// ...
+	// framework: '@storybook/react-webpack5', 👈 Remove this
+	framework: "@storybook/nextjs", // 👈 Add this
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+};
+
+export default config;
