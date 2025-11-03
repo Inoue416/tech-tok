7bd6a9e97250218fce29725f768847c40a297b1f
i-yuya <yuyatuta@gmail.com>
2025-11-03 16:19:49 +0900

    [ADD] CICDの設定を追加

---
+++ vercel-ignore-build-step.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
+
+# 許可するブランチのリスト
+ALLOWED_BRANCHE="develop"
+# マージコミットメッセージから元のブランチ名を抽出
+MERGE_BRANCH=$(echo "$VERCEL_GIT_COMMIT_MESSAGE" | grep -o "$ALLOWED_BRANCHE")
+echo "MERGE_BRANCH: $MERGE_BRANCH"
+
+# mainブランチへのマージかどうかを確認
+if [[ "$VERCEL_GIT_COMMIT_REF" == "release" ]]; then
+  if [ -n "$MERGE_BRANCH" ]; then
+    echo "✅ - ビルドを続行します (${MERGE_BRANCH}からのマージ)"
+    exit 1
+  else
+    echo "🛑 - ビルドをキャンセルしました (許可されていないブランチ)"
+    exit 0
+  fi
+else
+  echo "🛑 - releaseブランチではないためビルドをキャンセルしました"
+  exit 0
+fi
